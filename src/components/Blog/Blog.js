a48dfc1b8ad9769c5fd8ffc2bc5f2e42b1557aa2
Clover Sue <cloversbaxter@wccc.me.edu>
2022-11-28 05:45:26 +0000

    attempting to deploy

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -2,10 +2,8 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import BlogCard from "./BlogCards";
 import Particle from "../Particle";
-import zelda from "../../Assets/Projects/Zelda.jpg";
-import pet from "../../Assets/Projects/4-Leafs-Pet.jpg";
-import battle from "../../Assets/Projects/battlegame.jpg";
-import wd1 from "../../Assets/Projects/WD1.jpg";
+import logo from "../../Assets/FsUnicorn.png";
+
 
 function Blogs() {
   return (
@@ -13,19 +11,18 @@ function Blogs() {
       <Particle />
       <Container>
         <h1 className="blog-heading">
-          My Recent <strong className="purple">Essays </strong>
+          Under <strong className="purple">Construction </strong>
         </h1>
         <p style={{ color: "white" }}>
-          Here are a few college essays I've wrote recently.
+          Coming soon are a few college essays I've wrote recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="blog-card">
             <BlogCard
-              imgPath={zelda}
+              imgPath={logo}
               isBlog={false}
-              title="Zelda Like Game"
-              description="This is a Zelda styled game writen in Python. I followed along with a tutorial to research
-              how to make the virtual pet I wanted to make as my final project to MIT's 6.0001 course."
+              title="CloversTech Essays"
+              description="Coming Soon"
               ghLink="https://github.com/4-Leafs-Code/Like_Zelda_Game-"
               // demoLink="https://chatify-49.web.app/"
             />
@@ -33,14 +30,10 @@ function Blogs() {
 
           <Col md={4} className="blog-card">
             <BlogCard
-              imgPath={battle}
+              imgPath={logo}
               isBlog={false}
-              title="A React.js Battle Game"
-              description="This is a Turn-Based Battle Game built with React Only. 
-              You will be the character MagaMan.
-              Your opponent is Seamus.
-              You will have 1 of 3 choices per turn: Attack, Magic, or Heal.
-              When a character is defeated, the game over page will be displayed."
+              title="CloversTech Essays"
+              description="Coming Soon"
               ghLink="https://github.com/4-Leafs-Code/battlegame"
               // demoLink="https://blogs.soumya-jit.tech/"
             />
@@ -48,26 +41,11 @@ function Blogs() {
 
           <Col md={4} className="blog-card">
             <BlogCard
-              imgPath={pet}
+              imgPath={logo}
               isBlog={false}
-              title="4 Leafs Virtual Pet"
-              description="This virtual pet was writen to display a little of everything I had learned throughout 
-              MIT's 6.0001 course. The toolbar icons are just for looks at the moment but I hope to continue 
-              building this into a tamagotchi-like virtual pet."
-              ghLink="https://github.com/4-Leafs-Code/4-leafs-pet"
+              title="CloversTech Essays"
+              description="Coming Soon"
               // demoLink="https://editor.soumya-jit.tech/"              
-            />
-          </Col>
-
-          <Col md={4} className="blog-card">
-            <BlogCard
-              imgPath={wd1}
-              isBlog={false}
-              title="Website of my Design"
-              description="This is an update to the front-end design of a website I built. This version uses bootstrap 
-              while the backend was wrote with Python, Flask, and SQLAlchemy."
-              ghLink="https://github.com/4-Leafs-Code/IntroToWebDesignFP"
-              // demoLink="https://plant49-ai.herokuapp.com/"
             />
           </Col>
 
